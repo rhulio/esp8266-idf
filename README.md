5f68ca97bc4ad2b235abbe484cad981b2e07685d
rhulio <rhulio@2br.com.br>
2019-05-12 12:12:05 -0300

    comentários

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ESP8266-IDF
+/*
+
+RScada v0.1a
 Projeto teste para aprendizado da plataforma IDF no ESP8266. Plataforma: IDF
+Programação: IDF
+Microcontrolador: ESP8266
+
+*/
--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+/*
+
+RScada v0.1a
+Projeto teste para aprendizado da plataforma IDF no ESP8266. Plataforma: IDF
+Programação: IDF
+Microcontrolador: ESP8266
+
+*/
+
 #include "esp_common.h"
 #include "freertos/task.h"
 #include "gpio.h"
